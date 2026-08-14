67013834f784149e6b418c63af8b8b611ec69172
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-14 22:39:32 +0530

    Time: 5 ms (53.58%) | Memory: 78.2 MB (47.45%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+    //find the middle element
+    let slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+
+    //reverse the second half
+    let prev = null;
+    let curr = slow;
+    while (curr) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+
+
+
+    //move start and end pointer and compare each value
+    let firstList = head;
+    let secondList = prev;
+
+    while (secondList) {
+        if (firstList.val != secondList.val) {
+            return false;
+        }
+        firstList = firstList.next;
+        secondList = secondList.next;
+    }
+    return true;
+
+};
+
+
+
+
+
+
+
