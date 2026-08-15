e44cc5ee781fc410eb16fd974264033236c9144e
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2026-08-15 16:36:15 +0530

    Time: 3 ms (74.79%) | Memory: 57.1 MB (36.87%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function (s) {
+    let level = -1;
+    let ans = "";
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            ++level;
+            ans += (level ? s[i] : "")
+        } else {
+            ans += (level ? s[i] : "")
+            --level;
+        }
+    }
+    return ans;
+};
